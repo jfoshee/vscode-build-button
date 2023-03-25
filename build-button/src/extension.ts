0f447dea7eac4b46066cc6ae275ba0793c426c0e
Jacob Foshee <jacobf@gmail.com>
2023-03-25 08:42:08 -0500

    refactor: Remove unused code & use TaskGroup.Build rather than task.name === "build"
    I could not find a when clause that worked

--- build-button/src/extension.ts
+++ build-button/src/extension.ts
@@ -1,26 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerTextEditorCommand('extension.build', async (editor: vscode.TextEditor) => {
-    const tasks = await vscode.tasks.fetchTasks();
-    const buildTask = tasks.find(task => task.name === 'build');
-    if (buildTask) {
-      vscode.tasks.executeTask(buildTask);
-    }
-  });
-  context.subscriptions.push(disposable);
-
-  const buildButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-  buildButton.text = "$(gear) Build";
-  buildButton.command = 'extension.build';
-  buildButton.tooltip = 'Build the project';
-  buildButton.show();
-
-  const editorTitleButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -1);
-  editorTitleButton.text = "$(gear)";
-  editorTitleButton.command = 'extension.build';
-  editorTitleButton.tooltip = 'Build the project';
-  editorTitleButton.show();
+	const disposable = vscode.commands.registerTextEditorCommand('extension.build', async (editor: vscode.TextEditor) => {
+		const tasks = await vscode.tasks.fetchTasks();
+		const buildTask = tasks.find(task => task.group === vscode.TaskGroup.Build);
+		if (buildTask) {
+			vscode.tasks.executeTask(buildTask);
+		}
+	});
+	context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() { }
