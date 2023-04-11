f20b6b80af69292261aaf13aa60b9fc00b83bc45
Jacob Foshee <jacobf@gmail.com>
2023-04-11 10:03:29 -0500

    style: Tabs to spaces

--- build-button/src/extension.ts
+++ build-button/src/extension.ts
@@ -1,14 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerTextEditorCommand('extension.build', async (editor: vscode.TextEditor) => {
-		const tasks = await vscode.tasks.fetchTasks();
-		const buildTask = tasks.find(task => task.group === vscode.TaskGroup.Build);
-		if (buildTask) {
-			vscode.tasks.executeTask(buildTask);
-		}
-	});
-	context.subscriptions.push(disposable);
+  const disposable = vscode.commands.registerTextEditorCommand('extension.build', async (editor: vscode.TextEditor) => {
+    const tasks = await vscode.tasks.fetchTasks();
+    const buildTask = tasks.find(task => task.group === vscode.TaskGroup.Build);
+    if (buildTask) {
+      vscode.tasks.executeTask(buildTask);
+    }
+  });
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() { }
